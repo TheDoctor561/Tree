aad0bc3a388a79944b19537f7a6174f4cb89401d
Dan <daniel.yucua@gmail.com>
2022-07-07 21:34:26 -0400

    Tree Creation

---
+++ CreateTree.py
@@ -0,0 +1,41 @@
+class TreeNode: 
+  def __init__(self, data, children = []): 
+    self.data = data 
+    self.children = children
+
+  def __str__(self, level = 0): 
+    ret = "|_" * level + str(self.data) + "\n"
+
+    for child in self.children: 
+      ret += child.__str__(level + 1)
+    return ret 
+
+  def addChild(self, TreeNode): 
+    self.children.append(TreeNode)
+
+
+# Adding the original nodes onto the chain
+tree = TreeNode('Drinks', [])
+cold = TreeNode('Cold', [])
+hot = TreeNode('Hot', [])
+tree.addChild(cold)
+tree.addChild(hot)
+
+# add cola fanta and coffee to the cold/hot categories 
+
+# Hot Drinks 
+tea = TreeNode('Tea', [])
+coffee = TreeNode('Coffee', [])
+
+# Cold Drinks 
+Cola = TreeNode('Cola', [])
+Fanta = TreeNode('Fanta', [])
+
+hot.addChild(tea)
+hot.addChild(coffee)
+
+cold.addChild(Cola)
+cold.addChild(Fanta)
+
+
+print(tree)
